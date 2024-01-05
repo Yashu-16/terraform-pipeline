eb50b91f98b067b5cf09562de50adb600570f015
yashrandhe16 <yash@techwingsolutions.com>
2024-01-05 20:13:55 +0530

    code refactor

--- output.tf
+++ output.tf
@@ -2,9 +2,9 @@ output "id" {
     value = aws_instance.aws_instance[*].id
 }
 
-output "name" {
-    value = aws_instance.aws_instance[*].name
-}
+# output "name" {
+#     value = aws_instance.aws_instance[*].name
+# }
 
 output "public_ip" {
     value = aws_instance.aws_instance[*].public_ip
